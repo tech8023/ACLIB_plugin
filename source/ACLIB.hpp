1bf53b3a61e55502e4c0983ffde41e62e9f3fd18
styinx <zornchrisjo@gmail.com>
2021-04-18 14:35:23 +0200

    Move files containing non-python types to root dir

--- source/ACLIB.hpp
+++ source/ACLIB.hpp
@@ -5,8 +5,23 @@
 
 namespace ACLIB
 {
-    using Uint32 = std::int32_t;
-    using WChar  = wchar_t;
+    typedef std::uint32_t Uint32;
+    typedef std::int32_t  Sint32;
+    typedef wchar_t       WChar;
+
+    enum EVENT : Uint32
+    {
+        PHYSICS  = 0x00000000,
+        GEAR_CHANGED,
+        GRAPHICS = 0x10000000,
+        STATUS_CHANGED,
+        SESSION_CHANGED,
+        COMPLETED_LAPS_CHANGED,
+        POSITION_CHANGED,
+        IS_IN_PIT,
+        STATICS  = 0x20000000,
+    };
+
 }  // namespace ACLIB
 
 #endif  // ACLIB_PLUGIN_ACLIB_HPP
